8721b65524f393666fb3f6fad5648dc4457a7c59
lubegasimon <lubegasimon73@gmail.com>
2024-08-13 17:52:55 +0300

    server.ml -> app.ml

--- backend/app.ml
+++ backend/app.ml
@@ -1,5 +1,5 @@
-open Cohttp_lwt_unix
 open Lwt
+open Cohttp_lwt_unix
 
 let sanitize_path path =
   let segments = String.split_on_char '/' path in
@@ -28,7 +28,20 @@ let callback _conn req body =
       )
   | _ -> Server.respond_string ~headers ~status:`Not_found ~body:"Not found" ()
 
-let server = Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
+let server =
+  Lwt.catch
+    (fun () ->
+      (*FIXME: this is not printed on terminal when the server starts *)
+      let server =
+        Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
+      in
+      server >>= fun _ ->
+      Format.printf "Server listening on port 8000\n" |> Lwt.return)
+    (function
+      | Unix.Unix_error (err, func, arg) ->
+          Lwt_io.eprintf "Error starting server: %s in %s(%s)"
+            (Unix.error_message err) func arg
+      | exn -> Lwt_io.eprintlf "Unexpected error: %s" (Printexc.to_string exn))
 
 let () =
   Log.log;
